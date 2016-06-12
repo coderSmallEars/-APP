7caf94aa8d6cbb66237d6ee1ec8d3ddaf8733ea1
jinyulong <jin814138622@163.com>
2016-06-12 17:49:39 +0800

    support orientation

---
+++ URLPLayer/URLPLayer/Classes/SuperClasses/UPDrawerController.h
@@ -0,0 +1,13 @@
+//
+//  UPDrawerController.h
+//  URLPLayer
+//
+//  Created by jinyulong on 16/6/12.
+//  Copyright © 2016年 Player. All rights reserved.
+//
+
+#import <MMDrawerController/MMDrawerController.h>
+
+@interface UPDrawerController : MMDrawerController
+
+@end
---
+++ URLPLayer/URLPLayer/Classes/SuperClasses/UPDrawerController.m
@@ -0,0 +1,13 @@
+//
+//  UPDrawerController.m
+//  URLPLayer
+//
+//  Created by jinyulong on 16/6/12.
+//  Copyright © 2016年 Player. All rights reserved.
+//
+
+#import "UPDrawerController.h"
+
+@implementation UPDrawerController
+
+@end
